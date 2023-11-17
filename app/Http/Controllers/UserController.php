b8144b8c8dbc2aaf76822cd05a4dfb3e05f4594b
OsamaEid <osamaeidbm1993@gmail.com>
2023-11-17 16:18:24 +0200

    was

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -12,5 +14,43 @@ class UserController extends Controller
         return view('frontend.index');
 
 
+
+
     }
+
+
+    public function UserProfile(){
+
+        $id = Auth::user()->id;
+        $profileData = User::find($id);
+        return view('frontend.dashboard.edit_profile',compact('profileData'));
+
+    }// End Method 
+
+    public function UserStore(Request $request){
+
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+        $data->address = $request->address;
+
+        if($request->file('photo')){
+            $file = $request->file('photo');
+            @unlink(public_path('upload/user_images/'.$data->photo));
+            $filename = date('YmdHi').$file->getClientOriginalName();  
+            $file->move(public_path('upload/user_images'),$filename);
+            $data['photo'] = $filename;
+
+        }
+        $data->save();
+
+     
+        return redirect()->back()->with('success','profile updated successfully');
+
+    }// End Method 
+
+
+
 }
